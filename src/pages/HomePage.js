6c894dcf19bd7bf11f8a550c86cae877f150c15d
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-08-13 03:00:17 +0900

    add Search Feature

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,6 @@
-import { Layout } from 'antd';
+import { useState, useEffect } from 'react';
+import { Layout, Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import HeaderComponent from '../components/layout/HeaderComponent';
 import FooterComponent from '../components/layout/FooterComponent';
 import VideoCard from '../components/homePageComponent/VideoCard';
@@ -8,12 +10,44 @@ const { Content } = Layout;
 function HomePage() {
     const { data } = useApiRequest('http://localhost:8000/');
 
+    const [searchQuery, setSearchQuery] = useState('');
+    const [videos, setVideos] = useState(null);
+
+
+    const handleSearch = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const handleSearchQuery = async (event) => {
+        try {
+            const response = await fetch('http://localhost:8000/search?q=' + searchQuery);
+            if (response.ok) {
+                const data = await response.json();
+                setVideos(data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        handleSearchQuery();
+    });
+
+
     return (
         <Layout>
             <HeaderComponent />
+            <div style={{
+                background: '#28282F',
+                display: 'flex',
+                justifyContent: 'center',
+                padding: '20px 0px',
+            }}>
+                <Input name='search' onChange={handleSearch} value={searchQuery} size="large" placeholder="large size" prefix={<SearchOutlined />} allowClear style={{ width: '500px' }} />
+            </div>
             <Content style={{
                 padding: 24,
-                minHeight: 380,
                 background: '#28282F',
             }}>
                 <div style={{
@@ -21,16 +55,27 @@ function HomePage() {
                     flexWrap: 'wrap',
                     justifyContent: 'center',
                 }}>
-
-                    {data && data.videoList.map((video) => (
-                        <VideoCard
-                            id={video._id}
-                            key={video._id}
-                            title={video.title}
-                            image={video.imageThumbnailUrl}
-                            link={video.videoUrl}
-                        />
-                    ))}
+                    {searchQuery ?
+                        videos && videos.videoList.map((video) => (
+                            <VideoCard
+                                id={video._id}
+                                key={video._id}
+                                title={video.title}
+                                image={video.imageThumbnailUrl}
+                                link={video.videoUrl}
+                            />
+                        ))
+                        :
+                        data && data.videoList.map((video) => (
+                            <VideoCard
+                                id={video._id}
+                                key={video._id}
+                                title={video.title}
+                                image={video.imageThumbnailUrl}
+                                link={video.videoUrl}
+                            />
+                        ))
+                    }
                 </div>
             </Content>
             <FooterComponent />
